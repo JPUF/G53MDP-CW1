032404390b62c30073868c438fd818766059ffee
psyjb12 <psyjb12@nottingham.ac.uk>
2019-10-23 14:31:12 +0100

    SeekBar

--- app/src/main/java/com/example/g53mdp_cw1/BrushActivity.kt
+++ app/src/main/java/com/example/g53mdp_cw1/BrushActivity.kt
@@ -3,13 +3,38 @@ package com.example.g53mdp_cw1
 import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import android.widget.TextView
 
 class BrushActivity : AppCompatActivity() {
+
+    lateinit var sizeSeekBar: SeekBar
+    lateinit var sizeTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_brush)
 
+        sizeSeekBar = findViewById(R.id.sizeSeek)
+        sizeTextView = findViewById(R.id.sizeText)
+        sizeSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, i: Int, b: Boolean) {
+                val sizeValue = i + 1
+                Log.i("seekbar", "$sizeValue")
+                val textString = "Size: $sizeValue"
+                sizeTextView.text = textString
+            }
 
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
     }
 }
+
