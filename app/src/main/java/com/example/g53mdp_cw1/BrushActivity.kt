cde8b633e63dd50eb662230338fdfbaa33000f09
psyjb12 <psyjb12@nottingham.ac.uk>
2019-10-24 10:25:24 +0100

    Can set brush size.

--- app/src/main/java/com/example/g53mdp_cw1/BrushActivity.kt
+++ app/src/main/java/com/example/g53mdp_cw1/BrushActivity.kt
@@ -1,9 +1,12 @@
 package com.example.g53mdp_cw1
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.SeekBar
 import android.widget.TextView
 
@@ -11,18 +14,27 @@ class BrushActivity : AppCompatActivity() {
 
     lateinit var sizeSeekBar: SeekBar
     lateinit var sizeTextView: TextView
+    lateinit var bundle: Bundle
+    var brushSize: Int = 1
+    var textString = "Size: "
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_brush)
 
+        bundle = intent.extras!!
+        brushSize = bundle!!.getInt("brushSize")
+
         sizeSeekBar = findViewById(R.id.sizeSeek)
         sizeTextView = findViewById(R.id.sizeText)
+        textString = "Size: $brushSize"
+        sizeTextView.text = textString
+
         sizeSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, i: Int, b: Boolean) {
-                val sizeValue = i + 1
-                Log.i("seekbar", "$sizeValue")
-                val textString = "Size: $sizeValue"
+                brushSize = i + 1
+                Log.i("seekbar", "$brushSize")
+                textString = "Size: $brushSize"
                 sizeTextView.text = textString
             }
 
@@ -35,6 +47,14 @@ class BrushActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    fun returnToMain(view: View) {
+        val resultIntent = Intent()
+        bundle.putInt("brushSize", brushSize)
+        resultIntent.putExtras(bundle)
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
     }
 }
 
--- app/src/main/java/com/example/g53mdp_cw1/MainActivity.kt
+++ app/src/main/java/com/example/g53mdp_cw1/MainActivity.kt
@@ -1,22 +1,51 @@
 package com.example.g53mdp_cw1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.example.g53mdp_cw1.views.FingerPainterView
+
+const val BRUSH_ACTIVITY_REQUEST_CODE = 1
+const val COLOR_ACTIVITY_REQUEST_CODE = 2
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var painter: FingerPainterView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        painter = findViewById(R.id.painter)
     }
 
     fun goToBrush(view: View) {
-        startActivity(Intent(this, BrushActivity::class.java))
+        val bundle = Bundle()
+        bundle.putInt("brushSize", 1)
+        val brushIntent = Intent(this, BrushActivity::class.java)
+        brushIntent.putExtras(bundle)
+        startActivityForResult(brushIntent, BRUSH_ACTIVITY_REQUEST_CODE)
     }
 
     fun goToColor(view: View) {
         startActivity(Intent(this, ColorActivity::class.java))
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == BRUSH_ACTIVITY_REQUEST_CODE) {
+            if(resultCode == Activity.RESULT_OK) {
+                val bundle = data!!.extras
+                val brushSize = bundle!!.getInt("brushSize")
+                Log.i("MainActivityResult", "Brush Size: $brushSize")
+                painter.brushWidth = brushSize
+
+            }
+        }
+
     }
 }
